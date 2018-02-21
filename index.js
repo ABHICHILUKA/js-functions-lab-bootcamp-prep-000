6a909a47be349392ada24c171237272f5b0b2001
Nikhil Chiluka <chnikhil058@gmail.com>
2018-02-21 08:21:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
  function happyHolidays() {
    return("Happy holidays!")
  }
- function happyHolidaysTo($name){
+ function happyHolidaysTo(String){
    var c = `Happy holidays, ${name}!`
    return(c)
   }
-  function happyHolidayTo($holiday , $name){
+  function happyHolidayTo( String $holiday , String $name){
     var c =`Happy ${holiday}, ${name}!`
     return(c)
   }
-  function holidayCountdown( $days , $holiday){
+  function holidayCountdown( Number $days , string $holiday){
    var c = `It's ${days} days until ${holiday}!`
     return(c)
     
