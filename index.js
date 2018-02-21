287f29f82d4bc3d89646075f6c719b8876310a64
Nikhil Chiluka <chnikhil058@gmail.com>
2018-02-21 08:15:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
  function happyHolidays() {
-   return('Happy holidays!')
+   return("Happy holidays!")
  }
  function happyHolidaysTo($name){
-   return(Happy holidays, ${name})
+   return(`Happy holidays, ${name}`)
   }
   function happyHolidayTo($holiday , $name){
-    return('Happy ${holiday}, ${name}!')
+    return(`Happy ${holiday}, ${name}!`)
   }
+  function
